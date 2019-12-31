49a431ffd2dc17bbd3644bf90766fa041b884089
865332676@qq.com <865332676@qq.com>
2019-12-31 14:01:59 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  <h1>Congratulations on your!<br />Successful run vue in docker!</h1>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
